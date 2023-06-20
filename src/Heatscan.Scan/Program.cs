80af904e999b8a0985684f9cbce8d60b918760b7
Bas Nielander <Bas.Nielander@winvision.nl>
2023-06-20 15:59:45 +0200

    Console app (.net framework v4) toegevoegd om scans uit te lezen.

---
+++ src/Heatscan.Scan/Program.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.IO;
+
+using Flir.Atlas.Image;
+
+namespace Heatscan.Scan
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            if (args.Length != 1)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine("Argument (scan folder) missing");
+                return;
+            }
+
+            Console.WriteLine("GO Gadget");
+            try
+            {
+                var scanFolder = new DirectoryInfo(args[0]);
+                if (!scanFolder.Exists)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Argument ({args[0]}) is not an existing folder");
+                }
+
+                var scanImageFiles = scanFolder.GetFiles("*.jpg");
+
+                foreach (var imageFile in scanImageFiles)
+                {
+                    using (var file = new ThermalImageFile(imageFile.FullName))
+                    {
+                        file.Changed += (fileChange, args1) =>
+                        {
+                            Console.WriteLine("\tFile changed");
+                            Console.WriteLine(fileChange);
+                        };
+                        Console.WriteLine("=====================");
+                        Console.WriteLine(file.FileName);
+                        Console.WriteLine(file.MaxSignalValue);
+                        Console.WriteLine(file.MinSignalValue);
+                        Console.WriteLine(file.Scale.Range.Maximum);
+                        Console.WriteLine(file.Scale.Range.Minimum);
+                        file.Scale.ScaleImageChanged += (scaleChange, args2) =>
+                        {
+                            Console.WriteLine("\tScale changed");
+                            Console.WriteLine(scaleChange);
+                        };
+                        using (var scale = file.Scale)
+                        {
+                            scale.Range = new Range<double>(-13, 5);
+                        }
+
+                        Console.WriteLine("=====================");
+
+                        file.Save();
+                        file.Close();
+                    }
+                }
+            }
+            catch (Exception exception)
+            {
+                Console.ForegroundColor = ConsoleColor.Red;
+                Console.WriteLine(exception);
+            }
+        }
+    }
+}
